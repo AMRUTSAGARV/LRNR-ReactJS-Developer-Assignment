31ae6c5bcc85d20301f3f22120b611aed2d89c58
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-02 18:18:09 +0530

    getting started with npx-create-react-app & tailwindcss

---
+++ tailwin-webapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{ color: "yellow" }}>
+        TAILWIND CSS
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwin-webapp/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar of Our Application</div>
+  )
+}
+
+export default Navbar
