f64cc277f16c5f2d13eeb88b63cf7db850f7b38a
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-04 23:20:39 +0530

    mobile responsive navbar

--- tailwin-webapp/src/App.js
+++ tailwin-webapp/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header" style={{ color: "yellow" }}>
-        TAILWIND CSS
-      </header>
+      <Navbar />
     </div>
   );
 }
