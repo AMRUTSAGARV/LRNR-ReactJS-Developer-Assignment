c87efa23c0bd5ab40acba59b8be1734481425792
Amrut Sagar <amrutsagar06@gmail.com>
2022-10-10 19:04:43 +0530

    cards &footer added

--- tailwin-webapp/src/App.js
+++ tailwin-webapp/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
